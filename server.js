e157296a9d98ff620e34ac14ee188fa1a538c780
Cody <ydoc118@gmail.com>
2020-08-07 19:05:48 -0400

    added note data to push to db.json

--- server.js
+++ server.js
@@ -8,12 +8,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const notes = [];
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if(err) throw err;
+    const savedNote = JSON.stringify(data);
+    console.log(savedNote)
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  const data = JSON.stringify(notes)
+  fs.writeFile("db/db.json", data, (err) => {
+    if(err) throw err;
+    // console.log(data)
+  })
 })
 
 app.listen(PORT, function() {
