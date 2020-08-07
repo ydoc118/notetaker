3374fd0a4e708cfef400f834788cb2765248d68a
Cody <ydoc118@gmail.com>
2020-08-07 18:31:07 -0400

    removed the develop folder

---
+++ public/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+var fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/notes.html"))
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
