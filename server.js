de00d5afeef9ae9fd5f325490379a386772b067b
Cody <ydoc118@gmail.com>
2020-08-08 16:13:30 -0400

    fixed PORT to have process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 var fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
